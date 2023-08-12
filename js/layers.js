e4532d082a74ab5ec34393f5c41e989ef2080223
therealdumpsterfire <therealdumpsterfireepic@gmail.com>
2023-08-11 22:03:29 -0400

    mini-update #3

--- js/layers.js
+++ js/layers.js
@@ -1,36 +1,54 @@
 addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
+    name: "prestige", // This is optional, only used in a few places. If absent, it just uses the layer id.
+    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized.
+    position: 0, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    startData() {
+      return {
         unlocked: true,
-		points: new Decimal(0),
-    }},
+        points: new Decimal(0),
+      };
+    },
     color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account.
+    resource: "prestige points", // Name of the prestige currency.
+    baseResource: "points", // Name of the resource the prestige is based on.
+    baseAmount() {
+      return player.points;
+    }, // Get the current amount of baseResource.
+    type: "normal", // normal: cost to gain currency depends on the amount gained. static: cost depends on how much you already have.
+    exponent: 0.5, // Prestige currency exponent.
+    gainMult() {
+      // Calculate the multiplier for the main currency from bonuses.
+      mult = new Decimal(1);
+      
+      if (hasUpgrade('p', 11)) {
+        mult = mult.times(2); // Double the gain multiplier if the player has upgrade 11.
+      }
+      
+      return mult;
     },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
+    gainExp() {
+      // Calculate the exponent on the main currency from bonuses.
+      return new Decimal(1);
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 0, // Row the layer is in on the tree (0 is the first row).
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true},
-    upgrades: {
-        11: {
-            title: "test",
-            description: "Double your point gain.",
-            cost: new Decimal(1),
+      {
+        key: "p",
+        description: "P: Reset for prestige points",
+        onPress() {
+          if (canReset(this.layer)) doReset(this.layer);
         },
-    
-    }
-})
+      },
+    ],
+    layerShown() {
+      return true;
+    },
+    upgrades: {
+      11: {
+        title: "test",
+        description: "Double your point gain.",
+        cost: new Decimal(1),
+      },
+    },
+  });
